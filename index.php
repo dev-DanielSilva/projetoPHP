340efe1696892a6d74a1efaf6630db2592a6cd84
dev-DanielSilva <danibushin@gmail.com>
2024-09-21 15:46:10 -0300

    Alteração de Título

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="styles/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>Document</title>
+    <title>Manipulação de Imagens com PHP</title>
 </head>
 
 <body>
